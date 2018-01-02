9910275dee07328f3592ab7b6c6a9e79966d92cc
tanem <tane.morgan@gmail.com>
2018-01-03 07:08:31 +1300

    Tweak ci test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ "$CI" = true ]; then
-  jest --coverage
+  jest --coverage --coverageReporters=text-summary lcov
   cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js
 else
   jest "$@"
