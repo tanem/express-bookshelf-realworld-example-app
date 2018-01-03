5bc3910ee25c7460df9c58d0257a186805aaeb2c
tanem <tane.morgan@gmail.com>
2018-01-04 05:59:28 +1300

    Create comments migration

---
+++ data/migrations/20180104055140_create_comments.js
@@ -0,0 +1,20 @@
+'use strict';
+
+exports.up = knex =>
+  knex.schema.createTable('comments', table => {
+    table.increments();
+    table.timestamps();
+    table.text('body').notNullable();
+    table
+      .integer('author')
+      .notNullable()
+      .references('users.id')
+      .onDelete('CASCADE');
+    table
+      .integer('article')
+      .notNullable()
+      .references('articles.id')
+      .onDelete('CASCADE');
+  });
+
+exports.down = knex => knex.schema.dropTableIfExists('comments');
