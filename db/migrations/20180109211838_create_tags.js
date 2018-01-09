16c573d6f7002faed730aad9d48d857b45424097
tanem <tane.morgan@gmail.com>
2018-01-09 21:47:05 +1300

    Add tag module

---
+++ db/migrations/20180109211838_create_tags.js
@@ -0,0 +1,13 @@
+'use strict';
+
+exports.up = knex =>
+  knex.schema.createTable('tags', table => {
+    table.increments();
+    table.timestamps();
+    table
+      .string('name')
+      .notNullable()
+      .unique();
+  });
+
+exports.down = knex => knex.schema.dropTableIfExists('tags');
