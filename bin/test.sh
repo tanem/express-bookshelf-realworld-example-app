60748bfa32885d0ff3ab659a1ab2199aadcd498f
tanem <tane.morgan@gmail.com>
2018-02-02 06:55:47 +1300

    Fix test script

--- bin/test.sh
+++ bin/test.sh
@@ -4,7 +4,7 @@ set -e
 
 if [ "$CI" = true ]; then
   jest --coverage
-  cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js
+  cat ./_coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js
 else
   jest "$@"
 fi
