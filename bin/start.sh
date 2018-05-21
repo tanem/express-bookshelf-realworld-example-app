847080f881be05ed8ab7a283e33b3f114af6d0e0
tanem <tane.morgan@gmail.com>
2018-05-21 12:59:25 +1200

    Prettify log format

--- bin/start.sh
+++ bin/start.sh
@@ -6,5 +6,5 @@ if [ "$NODE_ENV" = "production" ]; then
   node lib/server.js
 else
   $(yarn bin)/knex --knexfile config/knexfile.js migrate:latest
-  $(yarn bin)/nodemon lib/server.js
+  $(yarn bin)/nodemon lib/server.js | $(yarn bin)/pino-pretty -c -t
 fi  
