79c50631904500f0a84df269857cb1b8142592bf
Tane Morgan <464864+tanem@users.noreply.github.com>
2026-03-15 14:26:06 +1300

    Cache table list in truncate for faster test runs

--- db/truncate.js
+++ db/truncate.js
@@ -1,16 +1,20 @@
 'use strict';
 
+let cachedTableNames;
+
 const truncate = async (knex) => {
-  const tables = await knex('pg_tables')
-    .select('tablename')
-    .where('schemaname', 'public');
+  if (!cachedTableNames) {
+    const tables = await knex('pg_tables')
+      .select('tablename')
+      .where('schemaname', 'public');
 
-  const tableNames = tables
-    .map((t) => t.tablename)
-    .filter((t) => !['knex_migrations', 'knex_migrations_lock'].includes(t))
-    .join(',');
+    cachedTableNames = tables
+      .map((t) => t.tablename)
+      .filter((t) => !['knex_migrations', 'knex_migrations_lock'].includes(t))
+      .join(',');
+  }
 
-  await knex.raw(`truncate table ${tableNames} restart identity`);
+  await knex.raw(`truncate table ${cachedTableNames} restart identity`);
 };
 
 module.exports = truncate;
